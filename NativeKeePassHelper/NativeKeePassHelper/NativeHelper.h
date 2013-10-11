222adfc1813c82992edf8ae9bed73cd9407c7682
Steven Fuqua <steven@sariph.net>
2013-10-10 21:22:36 -0700

    Added a delegate for checking the cancellation token. This callback is used by the native code to check on the cancellation status. The managed code checks it to return properly from the Tasks. Verified this does not cause a significant performance regression. Fixes #34.

--- NativeKeePassHelper/NativeKeePassHelper/NativeHelper.h
+++ NativeKeePassHelper/NativeKeePassHelper/NativeHelper.h
@@ -4,10 +4,12 @@ using Windows::Storage::Streams::IBuffer;
 
 namespace NativeKeePassHelper
 {
+	public delegate bool ConditionChecker();
+
     public ref class NativeHelper sealed
     {
     public:
 		// Native implementation of key transformation algorithm
-        static IBuffer^ TransformKey(uint64 numRounds, IBuffer^ key, IBuffer^ iv, IBuffer^ data);
+        static IBuffer^ TransformKey(uint64 numRounds, IBuffer^ key, IBuffer^ iv, IBuffer^ data, ConditionChecker^ checkCancel);
     };
 }
