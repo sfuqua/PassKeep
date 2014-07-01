b14e339e95e835908a24fc0f927cbab87ebce25b
Steven Fuqua <sfuqua@microsoft.com>
2014-07-01 01:28:01 -0700

    Quick cleanup of DashboardView.xaml.cs before moving on.

--- PassKeep/PassKeep.Windows/Views/DashboardView.xaml.cs
+++ PassKeep/PassKeep.Windows/Views/DashboardView.xaml.cs
@@ -1,26 +1,16 @@
-﻿using PassKeep.Common;
-using PassKeep.Framework;
+﻿using PassKeep.Framework;
 using PassKeep.Models;
 using PassKeep.ViewBases;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
 using Windows.ApplicationModel;
 using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.Storage;
-using Windows.Storage.AccessCache;
 using Windows.Storage.Pickers;
-using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Hub Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=321224
 
@@ -108,7 +98,6 @@ namespace PassKeep.Views
                 return;
             }
 
-            StorageApplicationPermissions.MostRecentlyUsedList.Add(pickedKdbx, pickedKdbx.Name);
             NavigateToOpenedFile(pickedKdbx);
         }
 
