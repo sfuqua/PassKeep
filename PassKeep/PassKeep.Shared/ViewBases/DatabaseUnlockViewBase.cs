5e38e7f1d318a2124978c356c6766f988496a3d3
Steven Fuqua <steven@sariph.net>
2014-07-13 22:41:48 -0700

    Added a caps lock warning to the unlock view. Fixes #36.

--- PassKeep/PassKeep.Shared/ViewBases/DatabaseUnlockViewBase.cs
+++ PassKeep/PassKeep.Shared/ViewBases/DatabaseUnlockViewBase.cs
@@ -4,19 +4,11 @@ using Windows.System;
 
 namespace PassKeep.ViewBases
 {
-    public class DatabaseUnlockViewBase : PassKeepPage<IDatabaseUnlockViewModel>
+    public abstract class DatabaseUnlockViewBase : PassKeepPage<IDatabaseUnlockViewModel>
     {
         public override bool HandleAcceleratorKey(VirtualKey key)
         {
             return false;
-        }
-
-        protected override void navigationHelper_LoadState(object sender, Common.LoadStateEventArgs e)
-        {
-        }
-
-        protected override void navigationHelper_SaveState(object sender, Common.SaveStateEventArgs e)
-        {
         }
     }
 }
