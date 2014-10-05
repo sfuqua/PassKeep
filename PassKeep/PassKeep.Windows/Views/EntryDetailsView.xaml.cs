12a6ec3db1c3f0ce3b33e66706f2262ba6e79e1f
Steven Fuqua <steven@sariph.net>
2014-10-05 00:49:32 -0700

    Implemented a proper "Save?" prompt for EntryDetailsView.

--- PassKeep/PassKeep.Windows/Views/EntryDetailsView.xaml.cs
+++ PassKeep/PassKeep.Windows/Views/EntryDetailsView.xaml.cs
@@ -8,6 +8,8 @@ using PassKeep.Views.Controls;
 using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Resources;
+using Windows.UI.Popups;
 
 // The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
 
@@ -18,10 +20,35 @@ namespace PassKeep.Views
     /// </summary>
     public sealed partial class EntryDetailsView : EntryDetailsViewBase
     {
+        private MessageDialog confirmationDialog;
+        private IUICommand confirmationYesCommand;
+        private IUICommand confirmationNoCommand;
+        private IUICommand confirmationCancelCommand;
+
         public EntryDetailsView()
             : base()
         {
             this.InitializeComponent();
+
+            // If we're eligible to show a prompt, do it!
+            this.confirmationDialog = new MessageDialog(
+                GetString("UnsavedPrompt"),
+                GetString("UnsavedPromptTitle")
+            )
+            {
+                Options = MessageDialogOptions.None,
+            };
+
+            this.confirmationYesCommand = new UICommand(GetString("Yes"));
+            this.confirmationNoCommand = new UICommand(GetString("No"));
+            this.confirmationCancelCommand = new UICommand(GetString("Cancel"));
+
+            this.confirmationDialog.Commands.Add(this.confirmationYesCommand);
+            this.confirmationDialog.Commands.Add(this.confirmationNoCommand);
+            this.confirmationDialog.Commands.Add(this.confirmationCancelCommand);
+
+            this.confirmationDialog.DefaultCommandIndex = 0;
+            this.confirmationDialog.CancelCommandIndex = 2;
         }
 
         public override bool HandleAcceleratorKey(Windows.System.VirtualKey key)
@@ -38,9 +65,35 @@ namespace PassKeep.Views
         {
         }
 
-        private void ConfirmedAction(Action callback)
+        /// <summary>
+        /// If the entry is dirty, prompts a user for a save before taking action.
+        /// </summary>
+        /// <param name="callback">The action to take if confirmed.</param>
+        private async Task PromptSaveAndThen(Action callback)
         {
-            if (true)
+            bool confirmed = this.ViewModel.IsReadOnly || !this.ViewModel.IsDirty();
+            if (!confirmed)
+            {
+                IUICommand chosenCmd = await this.confirmationDialog.ShowAsync();
+                if (chosenCmd == this.confirmationYesCommand)
+                {
+                    // User chose to save - continue only if it succeeds (is not cancelled)
+                    confirmed = await this.ViewModel.TrySave();
+                }
+                else if (chosenCmd == this.confirmationNoCommand)
+                {
+                    // User chose not to save - continue
+                    confirmed = true;
+                }
+                else
+                {
+                    // User chose to cancel, abort!
+                    Debug.Assert(chosenCmd == this.confirmationCancelCommand);
+                    confirmed = false;
+                }
+            }
+
+            if (confirmed)
             {
                 callback();
             }
@@ -51,12 +104,12 @@ namespace PassKeep.Views
         /// </summary>
         /// <param name="sender">The BreadcrumbNavigator.</param>
         /// <param name="e">EventArgs provided the clicked group.</param>
-        private void Breadcrumb_GroupClicked(object sender, GroupClickedEventArgs e)
+        private async void Breadcrumb_GroupClicked(object sender, GroupClickedEventArgs e)
         {
             IKeePassGroup clickedGroup = e.Group;
             Debug.Assert(clickedGroup != null);
 
-            ConfirmedAction(
+            await PromptSaveAndThen(
                 () => {
                     Debug.WriteLine("Updating View to breadcrumb: {0}", e.Group.Title.ClearValue);
                     this.ViewModel.NavigationViewModel.SetGroup(clickedGroup);
