bbf1b9537254ddcb473289e94f496f1b0b850a04
Steven Fuqua <sfuqua@microsoft.com>
2014-10-06 00:08:42 -0700

    Updated DatabaseView to save the ActiveGroup between navigations.

--- PassKeep/PassKeep.Shared/ViewBases/DatabaseViewBase.cs
+++ PassKeep/PassKeep.Shared/ViewBases/DatabaseViewBase.cs
@@ -1,11 +1,15 @@
 ﻿using PassKeep.Framework;
+using PassKeep.Lib.Contracts.Models;
 using PassKeep.Lib.Contracts.ViewModels;
+using System.Diagnostics;
 using Windows.System;
 
 namespace PassKeep.ViewBases
 {
     public abstract class DatabaseViewBase : PassKeepPage<IDatabaseViewModel>
     {
+        private const string ActiveGroupKey = "ActiveGroup";
+
         public override bool HandleAcceleratorKey(VirtualKey key)
         {
             return false;
@@ -13,10 +17,19 @@ namespace PassKeep.ViewBases
 
         protected override void navigationHelper_LoadState(object sender, Common.LoadStateEventArgs e)
         {
+            if (e.PageState != null && e.PageState.ContainsKey(DatabaseViewBase.ActiveGroupKey))
+            {
+                IKeePassGroup activeGroup = e.PageState[DatabaseViewBase.ActiveGroupKey] as IKeePassGroup;
+                Debug.Assert(activeGroup != null);
+
+                this.ViewModel.NavigationViewModel.SetGroup(activeGroup);
+            }
         }
 
         protected override void navigationHelper_SaveState(object sender, Common.SaveStateEventArgs e)
         {
+            e.PageState[DatabaseViewBase.ActiveGroupKey] =
+                this.ViewModel.NavigationViewModel.ActiveGroup;
         }
     }
 }
