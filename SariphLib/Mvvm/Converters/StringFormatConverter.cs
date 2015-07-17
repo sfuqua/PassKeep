e89f031898355c85a1422cfce83c4ca00b469217
Steven Fuqua <steven@sariph.net>
2015-07-17 00:06:48 -0700

    Fixed up the clipboard clear timer with a formatter and some cleaning. Fixes #10.

---
+++ SariphLib/Mvvm/Converters/StringFormatConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace SariphLib.Mvvm.Converters
+{
+    /// <summary>
+    /// A converter for formatting strings in bindings.
+    /// </summary>
+    public class StringFormatConverter : IValueConverter
+    {
+        /// <summary>
+        /// Formats <paramref name="value"/> with format string <paramref name="parameter"/>.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="language"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null)
+            {
+                return String.Empty;
+            }
+
+            if (parameter == null)
+            {
+                return value.ToString();
+            }
+
+            string strFormat = parameter.ToString();
+            return String.Format(strFormat, value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
