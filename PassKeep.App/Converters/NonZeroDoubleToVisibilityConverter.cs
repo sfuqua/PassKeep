77457f7eaf2cb9728d3972e5a543f3501d31aaa8
Steven Fuqua <sfuqua@microsoft.com>
2015-07-17 00:06:48 -0700

    Fixed up the clipboard clear timer with a formatter and some cleaning. Fixes #10.

---
+++ PassKeep.App/Converters/NonZeroDoubleToVisibilityConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace PassKeep.Converters
+{
+    /// <summary>
+    /// Converts a nonzero double to Visible, everything else to Collapsed.
+    /// </summary>
+    public sealed class NonZeroDoubleToVisibilityConverter : IValueConverter
+    {
+        /// <summary>
+        /// Attempts to convert a value, where not !0 is true and null is false.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="language"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            double? d = value as double?;
+            return (d.HasValue && d != 0 ? Visibility.Visible : Visibility.Collapsed);
+        }
+
+        /// <summary>
+        /// Not implemented - ConvertBack is undefined for this converter.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="language"></param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
