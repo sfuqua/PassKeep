2f540898471d62a68a20034cf8c4088aa284f8cb
Steven Fuqua <steven@sariph.net>
2015-07-18 01:11:20 -0700

    Moved FileSizeConverter to SariphLib and updated it to not depend on shlwapi. This will fix certification, and fixes #69.

---
+++ SariphLib/Mvvm/Converters/FileSizeConverter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Windows.ApplicationModel.Resources;
+using Windows.UI.Xaml.Data;
+
+namespace SariphLib.Mvvm.Converters
+{
+    /// <summary>
+    /// A converter for representing a file size (in bytes) as a clean string.
+    /// </summary>
+    public sealed class FileSizeConverter : IValueConverter
+    {
+        private readonly string[] ResourceKeys =
+        {
+            "Bytes",
+            "KibibyteSuffix",
+            "MebibyteSuffix",
+            "GibibyteSuffix",
+            "TebibyteSuffix"
+        };
+
+        private ResourceLoader resourceLoader;
+
+        public FileSizeConverter()
+        {
+            this.resourceLoader = ResourceLoader.GetForViewIndependentUse($"{nameof(SariphLib)}/Resources");
+        }
+
+        /// <summary>
+        /// Converts the specified number, in bytes, to a friendly file size string.
+        /// </summary>
+        /// <param name="value">The file size as a ulong, in bytes.</param>
+        /// <param name="targetType">String.</param>
+        /// <param name="parameter">Not used.</param>
+        /// <param name="language">Not used.</param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (targetType != typeof(String))
+            {
+                throw new ArgumentException("targetType must be String", "targetType");
+            }
+
+            ulong sizeInBytes = (value as ulong?) ?? 0;
+
+            int suffixIndex;
+            if (sizeInBytes != 0)
+            {
+                suffixIndex = System.Convert.ToInt32(
+                    Math.Min(
+                        Math.Floor(Math.Log(sizeInBytes, 1024)),
+                        ResourceKeys.Length - 1
+                    )
+                );
+            }
+            else
+            {
+                suffixIndex = 0;
+            }
+
+            string suffix = this.resourceLoader.GetString(this.ResourceKeys[suffixIndex]);
+            double units = sizeInBytes / Math.Pow(1024, suffixIndex);
+
+            if (suffixIndex > 0)
+            {
+                return $"{units:0.00} {suffix}";
+            }
+            else
+            {
+                // For bytes, we don't use a decimal
+                return $"{units} {suffix}";
+            }
+            
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
