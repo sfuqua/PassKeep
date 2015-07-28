005479941ad7a909791d11e07e9f5edc56532783
Steven Fuqua <sfuqua@microsoft.com>
2015-07-27 22:26:15 -0700

    Deleting PassKeep.Old at long last.

--- PassKeep.Old/Views/Bases/DatabaseViewBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using PassKeep.Controls;
-using PassKeep.Lib.Contracts.ViewModels;
-
-namespace PassKeep.Views.Bases
-{
-    public abstract class DatabaseViewBase : PassKeepPage<IDatabaseViewModel> { }
-}
