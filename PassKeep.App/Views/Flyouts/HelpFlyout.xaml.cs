16bf29f974576fde175e5c3c91b06ea6b5035c42
Steven Fuqua <steven@sariph.net>
2015-07-29 18:58:14 -0700

    Updated app to VS2015 RTM, fixed missing images. Added Reddit link to "Help".

--- PassKeep.App/Views/Flyouts/HelpFlyout.xaml.cs
+++ PassKeep.App/Views/Flyouts/HelpFlyout.xaml.cs
@@ -14,10 +14,16 @@ namespace PassKeep.Views.Flyouts
             InitializeComponent();
         }
 
-        private async void HyperlinkButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        private async void MailLink_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             Uri mailUri = new Uri($"mailto:{this.contactEmail.Text}?subject=PassKeep Feedback");
             await Launcher.LaunchUriAsync(mailUri);
+        }
+
+        private async void SubredditLink_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            Uri webUri = new Uri("https://www.reddit.com/r/PassKeep");
+            await Launcher.LaunchUriAsync(webUri);
         }
     }
 }
