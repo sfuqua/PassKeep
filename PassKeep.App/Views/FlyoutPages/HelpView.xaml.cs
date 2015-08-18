3f1b3f9f64f42d8bd173400ac7ea9b5aa5c8a77d
Steven Fuqua <steven@sariph.net>
2015-08-18 01:12:03 -0700

    Duplicated the help/settings flyouts into new views that are used instead when SettingsFlyout is not available (e.g. on phone). Fixes #102.

---
+++ PassKeep.App/Views/FlyoutPages/HelpView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using PassKeep.Framework;
+using System;
+using Windows.System;
+
+namespace PassKeep.Views.FlyoutPages
+{
+    public partial class HelpView : PassKeepPage
+    {
+        public HelpView()
+        {
+            InitializeComponent();
+        }
+
+        private async void MailLink_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            Uri mailUri = new Uri($"mailto:{this.contactEmail.Text}?subject=PassKeep Feedback");
+            await Launcher.LaunchUriAsync(mailUri);
+        }
+
+        private async void SubredditLink_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            Uri webUri = new Uri("https://www.reddit.com/r/PassKeep");
+            await Launcher.LaunchUriAsync(webUri);
+        }
+    }
+}
