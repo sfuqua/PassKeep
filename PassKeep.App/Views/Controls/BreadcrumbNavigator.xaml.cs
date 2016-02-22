0f6b479f46debd4b726dfd62ed0ccea1eab9f4cb
Steven Fuqua <steven@sariph.net>
2016-02-21 19:54:32 -0800

    File cleanup.

--- PassKeep.App/Views/Controls/BreadcrumbNavigator.xaml.cs
+++ PassKeep.App/Views/Controls/BreadcrumbNavigator.xaml.cs
@@ -7,8 +7,6 @@ using Windows.ApplicationModel.DataTransfer;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
-// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
-
 namespace PassKeep.Views.Controls
 {
     /// <summary>
@@ -17,28 +15,39 @@ namespace PassKeep.Views.Controls
     /// </summary>
     public sealed partial class BreadcrumbNavigator : UserControl
     {
-        public event EventHandler<GroupClickedEventArgs> GroupClicked;
-        private void RaiseGroupClicked(IKeePassGroup group)
-        {
-            if (GroupClicked != null)
-            {
-                GroupClicked(this, new GroupClickedEventArgs(group));
-            }
-        }
-
+        /// <summary>
+        /// Basic constructor.
+        /// </summary>
         public BreadcrumbNavigator()
         {
             this.InitializeComponent();
         }
 
+        /// <summary>
+        /// Dispatched when one of the breadcrumbs is clicked by the user.
+        /// </summary>
+        public event EventHandler<GroupClickedEventArgs> GroupClicked;
+
+        /// <summary>
+        /// Handler for click events on breadcrumbs - invokes <see cref="GroupClicked"/>.
+        /// </summary>
+        /// <param name="sender">The clicked breadcrumb.</param>
+        /// <param name="e">EventArgs for the click event.</param>
         private void breadcrumbs_ItemClick(object sender, ItemClickEventArgs e)
         {
             Debug.Assert(e.ClickedItem is Breadcrumb);
             RaiseGroupClicked((e.ClickedItem as Breadcrumb).Group);
         }
 
+        /// <summary>
+        /// Handler for drop events on breadcrumbs. Moves the dropped node into the target group,
+        /// if appropriate.
+        /// </summary>
+        /// <param name="sender">The element receiving the drop event.</param>
+        /// <param name="e">DragEventArgs for the drop.</param>
         private async void Breadcrumb_Drop(object sender, DragEventArgs e)
         {
+            // First get the group we are dropping onto...
             FrameworkElement senderElement = sender as FrameworkElement;
             Dbg.Assert(senderElement != null);
 
@@ -46,8 +55,11 @@ namespace PassKeep.Views.Controls
             Dbg.Assert(thisBreadcrumb != null);
 
             IKeePassGroup thisGroup = thisBreadcrumb.Group;
+            Dbg.Assert(thisGroup != null);
+
             DragOperationDeferral deferral = e.GetDeferral();
 
+            // Get the UUID of the dropped node - if possible, move it into this group.
             string encodedUuid = await e.DataView.GetTextAsync();
             if (thisGroup.TryAdopt(encodedUuid))
             {
@@ -64,8 +76,15 @@ namespace PassKeep.Views.Controls
             deferral.Complete();
         }
 
+        /// <summary>
+        /// Handler for DragEnter events on breadcrumbs. Updates the AcceptedOperation
+        /// property of <paramref name="e"/> based on the drop target.
+        /// </summary>
+        /// <param name="sender">The element receiving the DragEnter event.</param>
+        /// <param name="e">DragEventArgs for the drag.</param>
         private async void Breadcrumb_DragEnter(object sender, DragEventArgs e)
         {
+            // Get the group we are currently over...
             FrameworkElement senderElement = sender as FrameworkElement;
             Dbg.Assert(senderElement != null);
 
@@ -73,6 +92,10 @@ namespace PassKeep.Views.Controls
             Dbg.Assert(thisBreadcrumb != null);
 
             IKeePassGroup thisGroup = thisBreadcrumb.Group;
+            Dbg.Assert(thisGroup != null);
+
+            // Update the DataPackageOperation of the drag event based on whether
+            // we are dragging a node over a group (generally, yes).
             DragOperationDeferral deferral = e.GetDeferral();
 
             string text = await e.DataView.GetTextAsync();
@@ -84,14 +107,34 @@ namespace PassKeep.Views.Controls
 
             deferral.Complete();
         }
+
+        /// <summary>
+        /// Invokes the <see cref="GroupClicked"/> event.
+        /// </summary>
+        /// <param name="group">The group that was clicked.</param>
+        private void RaiseGroupClicked(IKeePassGroup group)
+        {
+            GroupClicked?.Invoke(this, new GroupClickedEventArgs(group));
+        }
     }
 
-    public class GroupClickedEventArgs : EventArgs
+    /// <summary>
+    /// Lightweight helped EventArgs class for clicked breadcrumb items.
+    /// </summary>
+    public sealed class GroupClickedEventArgs : EventArgs
     {
-        public IKeePassGroup Group { get; set; }
+        /// <summary>
+        /// Constructs an instance around <paramref name="group"/>.
+        /// </summary>
+        /// <param name="group">The clicked group.</param>
         public GroupClickedEventArgs(IKeePassGroup group)
         {
             Group = group;
         }
+
+        /// <summary>
+        /// The clicked group.
+        /// </summary>
+        public IKeePassGroup Group { get; set; }
     }
 }
