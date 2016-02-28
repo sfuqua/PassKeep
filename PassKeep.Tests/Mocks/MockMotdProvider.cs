0b8578b4fc65dbcc65facb2c957ca8ed7eacce03
Steven Fuqua <sfuqua@microsoft.com>
2016-02-28 13:41:40 -0800

    Added a test mock for IMotdProvider

---
+++ PassKeep.Tests/Mocks/MockMotdProvider.cs
@@ -0,0 +1,48 @@
+﻿using PassKeep.Lib.Contracts.Providers;
+
+namespace PassKeep.Tests.Mocks
+{
+    /// <summary>
+    /// Test class for <see cref="IMotdProvider"/>.
+    /// </summary>
+    public class MockMotdProvider : IMotdProvider
+    {
+        /// <summary>
+        /// True
+        /// </summary>
+        public bool ShouldDisplay
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// The string "Body".
+        /// </summary>
+        /// <returns>"Body"</returns>
+        public string GetBody()
+        {
+            return "Body";
+        }
+
+        /// <summary>
+        /// The string "Dismiss".
+        /// </summary>
+        /// <returns>"Dismiss"</returns>
+        public string GetDismiss()
+        {
+            return "Dismiss";
+        }
+
+        /// <summary>
+        /// The string "Title".
+        /// </summary>
+        /// <returns>"Title"</returns>
+        public string GetTitle()
+        {
+            return "Title";
+        }
+    }
+}
