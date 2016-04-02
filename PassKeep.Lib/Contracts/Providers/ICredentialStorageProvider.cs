c5492f57cfb8ab78efce61447859b036117f63c2
Steven Fuqua <sfuqua@microsoft.com>
2016-04-02 14:11:46 -0700

    Refactoring the CredentialService to a provider. Need tests and to build a service on top. Refs #22.

--- PassKeep.Lib/Contracts/Providers/ICredentialStorageProvider.cs
+++ PassKeep.Lib/Contracts/Providers/ICredentialStorageProvider.cs
@@ -1,29 +1,30 @@
-﻿using System.Threading.Tasks;
+﻿using PassKeep.Contracts.Models;
+using System.Threading.Tasks;
 using Windows.Storage.Streams;
 
-namespace PassKeep.Lib.Contracts.Services
+namespace PassKeep.Lib.Contracts.Providers
 {
     /// <summary>
     /// Provides a means of saving raw keys (pre-transformation)
     /// to some sort of secure storage, after verifying the identity
     /// of the user.
     /// </summary>
-    public interface ICredentialStorageService
+    public interface ICredentialStorageProvider
     {
         /// <summary>
         /// Asynchronously fetches data representing the raw
         /// aggregate key for a database.
         /// </summary>
-        /// <param name="databaseToken">Data identifying the key to fetch.</param>
+        /// <param name="database">Data identifying the key to fetch.</param>
         /// <returns>A task representing the key data.</returns>
-        Task<IBuffer> GetRawKeyAsync(string databaseToken);
+        Task<IBuffer> GetRawKeyAsync(IDatabaseCandidate database);
 
         /// <summary>
         /// Asynchronously stores the key for a database in a secure location.
         /// </summary>
-        /// <param name="databaseToken">Data identifying the key for future retrieval.</param>
+        /// <param name="database">Data identifying the key for future retrieval.</param>
         /// <param name="key">The key to store.</param>
-        /// <returns>A task that completes when storage has finished.</returns>
-        Task StoreRawKeyAsync(string databaseToken, IBuffer key);
+        /// <returns>A task representing whether the storage is successful.</returns>
+        Task<bool> TryStoreRawKeyAsync(IDatabaseCandidate database, IBuffer key);
     }
 }
