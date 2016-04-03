ae7f764ee60da2c444e2a74f05089d2a3cf7a12b
Steven Fuqua <steven@sariph.net>
2016-04-02 22:59:55 -0700

    Added credential service/provider references to DatabaseUnlockViewModel and updated tests. Refs #22.

---
+++ PassKeep.Tests/Mocks/MockIdentityVerifier.cs
@@ -0,0 +1,33 @@
+﻿using PassKeep.Lib.Contracts.Services;
+using System.Threading.Tasks;
+
+namespace PassKeep.Tests.Mocks
+{
+    /// <summary>
+    /// A configurable test implementation of <see cref="IIdentityVerificationService"/>.
+    /// </summary>
+    public sealed class MockIdentityVerifier : IIdentityVerificationService
+    {
+        public bool CanVerify { get; set; }
+
+        public bool Verified { get; set; }
+
+        /// <summary>
+        /// Asynchronously returns <see cref="CanVerify"/>.
+        /// </summary>
+        /// <returns></returns>
+        public Task<bool> CanVerifyIdentityAsync()
+        {
+            return Task.FromResult(CanVerify);
+        }
+
+        /// <summary>
+        /// Asynchronously returns <see cref="Verified"/>.
+        /// </summary>
+        /// <returns></returns>
+        public Task<bool> VerifyIdentityAsync()
+        {
+            return Task.FromResult(Verified);
+        }
+    }
+}
