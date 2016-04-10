bc8697d44c994396f4c82e2305a150f3584345f4
Steven Fuqua <steven@sariph.net>
2016-04-10 12:01:58 -0700

    Added a ViewModel over saved credentials, and tests. Refs #22. Still need to add UI for managing (deleting) saved credentials, and handling the "add" case when the vault is full.

---
+++ PassKeep.Lib/Contracts/ViewModels/ISavedCredentialsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using PassKeep.Contracts.Models;
+using SariphLib.Mvvm;
+using System.Collections.ObjectModel;
+
+namespace PassKeep.Lib.Contracts.ViewModels
+{
+    /// <summary>
+    /// Represents a ViewModel over a list of saved credentials, allowing
+    /// them to be managed/deleted.
+    /// </summary>
+    public interface ISavedCredentialsViewModel : IViewModel
+    {
+        /// <summary>
+        /// A command for deleting a specific credential.
+        /// </summary>
+        IAsyncCommand DeleteCredentialAsyncCommand { get; }
+
+        /// <summary>
+        /// A command for deleting all credentials.
+        /// </summary>
+        IAsyncCommand DeleteAllAsyncCommand { get; }
+
+        /// <summary>
+        /// A collection of credentials that are stored.
+        /// </summary>
+        ObservableCollection<string> CredentialTokens { get; }
+    }
+}
