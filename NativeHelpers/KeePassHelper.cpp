8ea7ea8728a45270b7762609506664401068775d
Steven Fuqua <steven@sariph.net>
2017-02-03 00:27:15 -0800

    Renamed native C++ files to prep for addition of native Argon2

---
+++ NativeHelpers/KeePassHelper.cpp
@@ -0,0 +1,76 @@
+﻿#include "pch.h"
+#include "KeePassHelper.h"
+#include "Rijndael.h"
+
+using namespace Platform;
+using Windows::Security::Cryptography::CryptographicBuffer;
+using namespace NativeHelpers;
+
+// Transforms data until cancellation is requested - useful for computing how many rounds fit into a given time span.
+uint64 KeePassHelper::TransformUntilCancelled(IBuffer^ key, IBuffer^ data, ConditionChecker^ cancellationToken)
+{
+    // Copy the AES key IBuffer to a Platform::Array
+    Array<unsigned char, 1U> ^keyArray = ref new Array<unsigned char, 1U>(key->Length);
+    CryptographicBuffer::CopyToByteArray(key, &keyArray);
+
+    // Copy the data to be encrypted to a Platform::Array and get a reference to the
+    // underlying data pointer
+    Array<unsigned char, 1U> ^dataArray = ref new Array<unsigned char, 1U>(data->Length);
+    CryptographicBuffer::CopyToByteArray(data, &dataArray);
+    unsigned char *dataBytes = dataArray->Data;
+
+    // Initialize the AES cipher
+    Rijndael aes;
+    aes.Init(Rijndael::ECB, Rijndael::EncryptDir, keyArray->Data, Rijndael::Key32Bytes, /* iv */ 0);
+
+    uint64 rounds;
+    for (rounds = 1; rounds < 0xffffffffffffffff; rounds++) {
+        if (cancellationToken()) {
+            break;
+        }
+
+        aes.BlockEncrypt(dataBytes, 128, dataBytes);
+    }
+
+    return rounds;
+}
+
+// Native implementation of the KeePass key transformation algorithm, to be
+// called from managed code as a performance improvement.
+IBuffer^ KeePassHelper::TransformKey(uint64 numRounds, IBuffer^ key, IBuffer^ iv, IBuffer^ data, ConditionChecker^ cancellationToken)
+{
+    // Copy the AES key IBuffer to a Platform::Array
+    Array<unsigned char, 1U> ^keyArray = ref new Array<unsigned char, 1U>(key->Length);
+    CryptographicBuffer::CopyToByteArray(key, &keyArray);
+
+    // Copy the AES IV to a Platform::Array - if appropriate
+    // The typical case for KeePass is to pass in null/0
+    bool hasIv = iv != nullptr;
+    Array<unsigned char, 1U> ^ivArray;
+    if (hasIv) {
+         ivArray = ref new Array<unsigned char, 1U>(iv->Length);
+        CryptographicBuffer::CopyToByteArray(iv, &ivArray);
+    }
+
+    // Copy the data to be encrypted to a Platform::Array and get a reference to the
+    // underlying data pointer
+    Array<unsigned char, 1U> ^dataArray = ref new Array<unsigned char, 1U>(data->Length);
+    CryptographicBuffer::CopyToByteArray(data, &dataArray);
+    unsigned char *dataBytes = dataArray->Data;
+
+    // Initialize the AES cipher
+    Rijndael aes;
+    aes.Init(Rijndael::ECB, Rijndael::EncryptDir, keyArray->Data, Rijndael::Key32Bytes, (hasIv ? ivArray->Data : 0));
+
+    for (uint64 i = 0; i < numRounds; i++) {
+        if (cancellationToken()) {
+            break;
+        }
+
+        // Encrypt the data - CryptographicEngine::Encrypt is *too slow*
+        aes.BlockEncrypt(dataBytes, 128, dataBytes);
+    }
+
+    // Return an IBuffer back to the managed code
+    return CryptographicBuffer::CreateFromByteArray(dataArray);
+}
---
+++ NativeHelpers/KeePassHelper.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+using Windows::Storage::Streams::IBuffer;
+
+namespace NativeHelpers
+{
+    public delegate bool ConditionChecker();
+
+    // Managed helper class for KeePass algorithms
+    public ref class KeePassHelper sealed
+    {
+    public:
+        // Transforms data until cancellation is requested - useful for computing how many rounds fit into a given time span.
+        static uint64 TransformUntilCancelled(IBuffer^ key, IBuffer^ data, ConditionChecker^ cancellationToken);
+
+        // Native implementation of key transformation algorithm
+        static IBuffer^ TransformKey(uint64 numRounds, IBuffer^ key, IBuffer^ iv, IBuffer^ data, ConditionChecker^ cancellationToken);
+    };
+}
