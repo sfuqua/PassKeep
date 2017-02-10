543e3345c8af3ffc4f8b8463fe2e5678e1047520
Steven Fuqua <sfuqua@microsoft.com>
2017-02-10 00:50:06 -0800

    Finished a parallel, tested implementation of Argon2d. Refs #169. Just need to integrate with the parser code.

--- PassKeep.Tests/Argon2Tests.cs
+++ PassKeep.Tests/Argon2Tests.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
 using PassKeep.Lib.KeePass.Crypto;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace PassKeep.Tests
 {
@@ -10,7 +12,7 @@ namespace PassKeep.Tests
         /// Test vector from the RFC over 32 KiB with Parallelism = 4.
         /// </summary>
         [TestMethod]
-        public void Argon2dRfcVector()
+        public async Task Argon2dRfcVector()
         {
             byte[] password = new byte[32];
             for (int i = 0; i < 32; i++)
@@ -48,7 +50,7 @@ namespace PassKeep.Tests
             );
 
             byte[] output = new byte[32];
-            engine.HashAsync(output);
+            await engine.HashAsync(output, CancellationToken.None);
 
             byte[] expectedOutput = new byte[32]
             {
@@ -65,10 +67,65 @@ namespace PassKeep.Tests
         }
 
         /// <summary>
+        /// Test vector with segment lengths of 4+.
+        /// </summary>
+        [TestMethod]
+        public async Task Argon2LongerSegments()
+        {
+            byte[] password = new byte[32];
+            for (int i = 0; i < 32; i++)
+            {
+                password[i] = 1;
+            }
+
+            byte[] salt = new byte[16];
+            for (int i = 0; i < 16; i++)
+            {
+                salt[i] = 2;
+            }
+
+            byte[] secret = new byte[8];
+            for (int i = 0; i < 8; i++)
+            {
+                secret[i] = 3;
+            }
+
+            byte[] data = new byte[12];
+            for (int i = 0; i < 12; i++)
+            {
+                data[i] = 4;
+            }
+
+            Argon2d engine = new Argon2d(
+                password,
+                salt,
+                secret,
+                data,
+                2,
+                8,
+                32,
+                5
+            );
+
+            byte[] output = new byte[8];
+            await engine.HashAsync(output, CancellationToken.None);
+
+            byte[] expectedOutput = new byte[8]
+            {
+                0xa4, 0x26, 0xcc, 0xef, 0x1b, 0x72, 0x4a, 0xaf
+            };
+
+            for (int i = 0; i < 8; i++)
+            {
+                Assert.AreEqual(expectedOutput[i], output[i], $"out[{i}]");
+            }
+        }
+
+        /// <summary>
         /// A basic test with Parallelism = 1 over 8 Kb of memory.
         /// </summary>
         [TestMethod]
-        public void Argon2SimpleSingleThread()
+        public async Task Argon2SimpleSingleThread()
         {
             byte[] password = new byte[16];
             for (int i = 0; i < 16; i++)
@@ -94,12 +151,68 @@ namespace PassKeep.Tests
             );
 
             byte[] output = new byte[16];
-            engine.HashAsync(output);
+            await engine.HashAsync(output, CancellationToken.None);
 
             byte[] expectedOutput = new byte[16]
             {
                 0x6d, 0x1a, 0x2c, 0x5f, 0x65, 0x4a, 0x27, 0x7e,
                 0x03, 0x9e, 0xad, 0xe2, 0x03, 0x68, 0x21, 0x61
+            };
+
+            for (int i = 0; i < 16; i++)
+            {
+                Assert.AreEqual(expectedOutput[i], output[i], $"out[{i}]");
+            }
+        }
+
+        /// <summary>
+        /// Test vector from the RFC over 1024 KiB with Parallelism = 8.
+        /// </summary>
+        [TestMethod]
+        public async Task Argon2HeavyMultiThread()
+        {
+            byte[] password = new byte[32];
+            for (int i = 0; i < 32; i++)
+            {
+                password[i] = 1;
+            }
+
+            byte[] salt = new byte[16];
+            for (int i = 0; i < 16; i++)
+            {
+                salt[i] = 2;
+            }
+
+            byte[] secret = new byte[8];
+            for (int i = 0; i < 8; i++)
+            {
+                secret[i] = 3;
+            }
+
+            byte[] data = new byte[12];
+            for (int i = 0; i < 12; i++)
+            {
+                data[i] = 4;
+            }
+
+            Argon2d engine = new Argon2d(
+                password,
+                salt,
+                secret,
+                data,
+                8,
+                16,
+                40960,
+                8
+            );
+
+            byte[] output = new byte[16];
+            await engine.HashAsync(output, CancellationToken.None);
+
+            byte[] expectedOutput = new byte[16]
+            {
+                0x23, 0xb8, 0x9e, 0x2f, 0x01, 0xce, 0x60, 0xa1,
+                0xd3, 0x2b, 0xe8, 0xa0, 0x92, 0x64, 0xc2, 0x80
             };
 
             for (int i = 0; i < 16; i++)
