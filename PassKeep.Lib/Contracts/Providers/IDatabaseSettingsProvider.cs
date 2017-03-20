67ce8bc4e8f7224ee9fa03b08d94ae8a381eae01
Steven Fuqua <steven@sariph.net>
2017-03-20 08:34:03 -0700

    Some initial KDBX4 UI infrastructure work. Update to VS2017, add editorconfig.

---
+++ PassKeep.Lib/Contracts/Providers/IDatabaseSettingsProvider.cs
@@ -0,0 +1,21 @@
+﻿using PassKeep.Lib.Contracts.KeePass;
+using PassKeep.Lib.KeePass.Kdf;
+
+namespace PassKeep.Lib.Contracts.Providers
+{
+    /// <summary>
+    /// Allows the user to configure settings for a specific database.
+    /// </summary>
+    public interface IDatabaseSettingsProvider
+    {
+        /// <summary>
+        /// Configures the cipher used to encrypt the database.
+        /// </summary>
+        EncryptionAlgorithm Cipher { get; set; }
+
+        /// <summary>
+        /// Configures the parameters used to transform the key.
+        /// </summary>
+        KdfParameters KdfParameters { get; set; }
+    }
+}
---
+++ PassKeep.Lib/ViewModels/DatabaseSettingsViewModel.cs
@@ -0,0 +1,113 @@
+﻿using PassKeep.Lib.Contracts.KeePass;
+using PassKeep.Lib.Contracts.Providers;
+using PassKeep.Lib.Contracts.ViewModels;
+using PassKeep.Lib.KeePass.Kdf;
+using SariphLib.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassKeep.Lib.ViewModels
+{
+    /// <summary>
+    /// Exposes KDF and cipher settings to the view.
+    /// </summary>
+    public class DatabaseSettingsViewModel : AbstractViewModel, IDatabaseSettingsViewModel
+    {
+        private readonly IDatabaseSettingsProvider settingsProvider;
+
+        private AesParameters aesParams;
+        private Argon2Parameters argonParams;
+
+        public DatabaseSettingsViewModel(IDatabaseSettingsProvider settingsProvider)
+        {
+            if (settingsProvider == null)
+            {
+                throw new ArgumentNullException(nameof(settingsProvider));
+            }
+
+            this.settingsProvider = settingsProvider;
+        }
+
+        /// <summary>
+        /// Algorithm to use for encrypting the database.
+        /// </summary>
+        public EncryptionAlgorithm Cipher
+        {
+            get { return this.settingsProvider.Cipher; }
+            set
+            {
+                if (Cipher != value)
+                {
+                    this.settingsProvider.Cipher = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Identifier for the key derivation function to use.
+        /// </summary>
+        public Guid KdfGuid
+        {
+            get { return this.settingsProvider.KdfParameters.Uuid; }
+            set
+            {
+                if (KdfGuid != value)
+                {
+                    if (value.Equals(Argon2Parameters.Argon2Uuid))
+                    {
+                        this.settingsProvider.KdfParameters = this.argonParams;
+                    }
+                    else if (value.Equals(AesParameters.AesUuid))
+                    {
+                        this.settingsProvider.KdfParameters = this.aesParams;
+                    }
+                    else
+                    {
+                        Dbg.Assert(false);
+                    }
+
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Number of transform rounds to use for the KDF.
+        /// </summary>
+        int KdfIterations
+        {
+            get
+            {
+                if (KdfGuid == this.argonParams.Uuid)
+                {
+                    return this.argonParams.tran
+                }
+            }
+            set;
+        }
+
+        /// <summary>
+        /// If Argon2 is the <see cref="Cipher"/>, configures the degree
+        /// of parallelism.
+        /// </summary>
+        int ArgonParallelism
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// If Argon2 is the <see cref="Cipher"/>, configures the amount of
+        /// memory used.
+        /// </summary>
+        int ArgonBlockCount
+        {
+            get;
+            set;
+        }
+    }
+}
