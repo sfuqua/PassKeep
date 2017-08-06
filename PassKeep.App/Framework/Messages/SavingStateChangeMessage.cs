1618b22ebf5ee85cb38f78fe455e297f13fd84fa
Steven Fuqua <steven@sariph.net>
2017-08-06 12:29:02 -0700

    Code hygeine and licensing. Fixes #192.

--- PassKeep.App/Framework/Messages/SavingStateChangeMessage.cs
+++ PassKeep.App/Framework/Messages/SavingStateChangeMessage.cs
@@ -1,4 +1,8 @@
-﻿namespace PassKeep.Framework.Messages
+﻿// Copyright 2017 Steven Fuqua
+// This file is part of PassKeep and is licensed under the GNU GPL v3.
+// For the full license, see gpl-3.0.md in this solution or under https://bitbucket.org/sapph/passkeep/src
+
+namespace PassKeep.Framework.Messages
 {
     /// <summary>
     /// A message that represents a change in whether a database is
