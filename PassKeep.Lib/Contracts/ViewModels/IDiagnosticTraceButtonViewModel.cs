0b078f7e75d5cfec15c45b62678334ee1a1f097f
Steven Fuqua <steven@sariph.net>
2017-11-26 22:25:39 -0800

    Initial work on a 'start trace' button

---
+++ PassKeep.Lib/Contracts/ViewModels/IDiagnosticTraceButtonViewModel.cs
@@ -0,0 +1,25 @@
+﻿using SariphLib.Mvvm;
+
+namespace PassKeep.Lib.Contracts.ViewModels
+{
+    /// <summary>
+    /// Represents a button that can start/stop a trace to a file.
+    /// </summary>
+    public interface IDiagnosticTraceButtonViewModel : IViewModel
+    {
+        /// <summary>
+        /// Whether a trace is currently running.
+        /// </summary>
+        bool IsTracing { get; }
+
+        /// <summary>
+        /// The label to display to the view based on the current state.
+        /// </summary>
+        string Label { get; }
+
+        /// <summary>
+        /// Starts/stops a trace based on the current state.
+        /// </summary>
+        IAsyncCommand Command { get; }
+    }
+}
