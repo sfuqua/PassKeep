f12263592bc2fd10a7ac0a98633621a5c6967945
Steven Fuqua <sfuqua@microsoft.com>
2018-04-15 23:47:01 -0700

    Missed files in last commit

---
+++ PassKeep.Lib/Contracts/Providers/IMasterKeyChangeViewModelFactory.cs
@@ -0,0 +1,11 @@
+﻿using PassKeep.Lib.Contracts.ViewModels;
+using PassKeep.Lib.KeePass.Dom;
+using SariphLib.Files;
+
+namespace PassKeep.Lib.Contracts.Providers
+{
+    public interface IMasterKeyChangeViewModelFactory
+    {
+        IMasterKeyViewModel Assemble(KdbxDocument document, ITestableFile databaseFile);
+    }
+}
---
+++ PassKeep.Lib/Providers/MasterKeyChangeViewModelFactory.cs
@@ -0,0 +1,27 @@
+﻿using PassKeep.Lib.Contracts.Providers;
+using PassKeep.Lib.Contracts.Services;
+using PassKeep.Lib.Contracts.ViewModels;
+using PassKeep.Lib.KeePass.Dom;
+using PassKeep.Lib.ViewModels;
+using SariphLib.Files;
+using System;
+
+namespace PassKeep.Lib.Providers
+{
+    public class MasterKeyChangeViewModelFactory : IMasterKeyChangeViewModelFactory
+    {
+        private readonly IDatabaseCredentialProvider credentialProvider;
+        private readonly IFileAccessService fileAccessService;
+
+        public MasterKeyChangeViewModelFactory(IDatabaseCredentialProvider credentialProvider, IFileAccessService fileAccessService)
+        {
+            this.credentialProvider = credentialProvider ?? throw new ArgumentNullException(nameof(credentialProvider));
+            this.fileAccessService = fileAccessService ?? throw new ArgumentNullException(nameof(fileAccessService));
+        }
+
+        public IMasterKeyViewModel Assemble(KdbxDocument document, ITestableFile databaseFile)
+        {
+            return new MasterKeyChangeViewModel(document, databaseFile, this.credentialProvider, this.fileAccessService);
+        }
+    }
+}
